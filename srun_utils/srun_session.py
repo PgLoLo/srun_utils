179a224b6ee464e90d91c99dd72c19436963b371
PG_LoLo <novgosh@yandex.ru>
2023-10-15 17:20:08 +0300

    add more options for srn

--- srun_utils/srun_session.py
+++ srun_utils/srun_session.py
@@ -4,13 +4,26 @@ from pathlib import Path
 
 
 class SrunSession:
-    def __init__(self, runs_dir: str | Path, time: str, job_name: str):
+    def __init__(
+        self,
+        runs_dir: str | Path,
+        time: str,
+        job_name: str,
+        n_gpus: int,
+        n_cpus: int,
+        mem: int,
+        partition: str,
+    ):
         runs_dir = Path(runs_dir).expanduser()
         assert runs_dir.exists(), f'Dirrectory with runs {runs_dir} does not exist'
 
         self.runs_dir = runs_dir
         self.time = time
         self.job_name = job_name
+        self.n_gpus = n_gpus
+        self.n_cpus = n_cpus
+        self.mem = mem
+        self.partition = partition
 
     def srun(self, command: str) -> None:
         work_folder = self.runs_dir / str(uuid.uuid4())
@@ -21,6 +34,10 @@ class SrunSession:
                 'sbatch',
                 f'--time={self.time}',
                 f'--job-name={self.job_name}',
+                '-p', self.partition,
+                '-G', f'{self.n_gpus}',
+                '-c', f'{self.n_cpus}',
+                '--mem', f'{self.mem}G'
                 '--output=%j.stdout',
                 '--error=%j.stderr',
                 '--wrap',
