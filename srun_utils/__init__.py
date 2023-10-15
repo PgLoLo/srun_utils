83afad8da6d96a76a70d7489bbd3797d952cd6f1
PG_LoLo <novgosh@yandex.ru>
2023-10-15 16:47:59 +0300

    fix naming issues

--- srun_utils/__init__.py
+++ srun_utils/__init__.py
@@ -1,12 +1,12 @@
 from pathlib import Path
 
-from spm_utils.srun_session import SrunSession
+from srun_utils.srun_session import SrunSession
 
 
 __all__ = ['SrunSession', 'srun', 'init']
 
 
-def init(runs_dir: Path, time: str, job_name: str) -> None:
+def init(runs_dir: str | Path, time: str, job_name: str) -> None:
     global SRUN_SESSION
     SRUN_SESSION = SrunSession(runs_dir, time, job_name)
 
